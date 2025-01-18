0dee9e6d5136950df2f6f581e0486207f3e3ea48
Praveen <praveenpatidar171@gmail.com>
2025-01-18 13:09:54 +0530

    changes finished

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -19,7 +19,7 @@ export const HeroSection = () => {
         <div className="flex flex-col gap-5 my-10">
             <span className="mx-auto px-4 py-2 rounded-full bg-gray-100 text-[#F83002] font-medium" >No. 1 Job Hunt Website</span>
             <h1 className="text-5xl font-bold">Search, Apply & <br /> Get Your <span className="text-[#6A38C2]">Dream Jobs</span></h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum doloremque vero porro et repellat?</p>
+            <p>Discover exciting career opportunities and take the next big step toward your dream job!</p>
             <div className="flex items-center rounded-md mx-auto w-[40%] shadow-lg">
                 <Input onChange={(e) => setQuery(e.target.value)} type='text' placeholder='Find your dream jobs' className='w-full outline-none focus-visible:ring-transparent' />
                 <Button onClick={searchHandler} className='bg-[#6A38C2]'><Search className="h-5 w-5" /></Button>
